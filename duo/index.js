1f32b7fc15fd80bde47d1300bba52eb79c9d4b00
John K. Paul <john@johnkpaul.com>
2014-06-21 19:08:44 -0400

    Change duo index to run through every user for cron

--- duo/index.js
+++ duo/index.js
@@ -1,14 +1,25 @@
 var request = require('./request');
 var client = require('./twilio');
 
-var to = require('../config/secrets.js').to;
+var users = require('../config/secrets.js').users;
 
-request('johnkpaul').then(function(json){
-  var finishedToday = json.streak_extended_today;
-  if(!finishedToday){
-    client.send(to, "Remember to Duolingo today!");
-  }
-  else {
-    client.send(to, "Good job Duolingoing!");
-  }
+users.forEach(function(user){
+  var to = user.phone_number;
+
+  request(user.duolingo_handle).then(function(json){
+    var finishedToday = json.streak_extended_today;
+    if(!finishedToday){
+      client.send(to, "Remember to Duolingo today!").then(function(message){
+        console.log(JSON.stringify(message));
+        console.log('Hope to be proud of' + user.duolingo_handle);
+      });
+    }
+    else {
+      client.send(to, "Good job Duolingoing!").then(function(){
+        console.log(JSON.stringify(message));
+        console.log('Be proud of' + user.duolingo_handle);
+      });;
+    }
+  });
+
 });
