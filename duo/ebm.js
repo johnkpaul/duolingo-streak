b6674e2a5c3c0cfdcba56e9be8b1aaafddb7f0d8
John K. Paul <john@johnkpaul.com>
2018-08-22 19:01:45 -0400

    Updates

--- duo/ebm.js
+++ duo/ebm.js
@@ -14,23 +14,27 @@ var me = users.find(function(u){ return u.duolingo_handle === "johnkpaul"; })
 
 
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = devices['iPhone 6'];
 
   (async () => { 
     let text = '';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.emulate(iPhone);
+    await delay(Math.floor(Math.random()*100000));
+    
     await page.goto('https://www.facebook.com/HM4HBNY/posts_to_page/');
     try {
     //await page.waitForSelector("#pages_posts_to_page_pagelet");
 
-          await delay(1000);
-          text = await page.evaluate(() => {
-          return document.getElementById('pages_posts_to_page_pagelet').textContent;
-        });
+          text = await page.evaluate(() => {return document.getElementsByClassName('story_body_container')[0].textContent; });
     }
     catch(e){
       await page.screenshot({path: '/tmp/screenshot.png'})
-      console.log(e);
+      client.send(me.phone_number, "FB blocked").then(function(message){
+        console.log(JSON.stringify(message));
+      });
       browser.close();
       return;
     }
@@ -43,10 +47,13 @@ const puppeteer = require('puppeteer');
       if (stringsAreDifferent(oldValue, text)){
           console.log('changed from: ' + oldValue + ' to ' + text);
           db.set('lastFBText', text).write();
-          client.send(me.phone_number, "There was an update on HM4HBNY \n\n" + text.substring(0, 100)).then(function(message){
-            console.log(JSON.stringify(message));
-            console.log("sent message to phone");
-          });
+          users.forEach(function(me){
+              client.send(me.phone_number, "There was an update on HM4HBNY \n\n" + text).then(function(message){
+                console.log(JSON.stringify(message));
+                console.log("sent message to phone");
+              });
+          
+          })
       } else {
         //console.log('same');
       }
