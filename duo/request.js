95aa54596006b199de2dc6bfdae7a820388214da
John K. Paul <john@johnkpaul.com>
2019-02-02 09:13:38 -0500

    Add chrome user agent

--- duo/request.js
+++ duo/request.js
@@ -2,7 +2,11 @@ var BlueBird = require("bluebird");
 var promisifiedRequest = BlueBird.promisify(require('request'));
 
 module.exports = function(username){
-  return promisifiedRequest('https://www.duolingo.com/users/' + username).then(function(message){
+  return promisifiedRequest('http://api.duolingo.com/users/' + username, {
+            headers: {
+              'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+            }
+          }).then(function(message){
     var body = message[1];
     return JSON.parse(body);
   });
